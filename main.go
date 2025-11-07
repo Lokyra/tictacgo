935dbdeb91aed7d50ffd335e0215098fb82a37a7
Lokyra <ayoub.bousbia@proton.me>
2025-11-07 14:41:09 +0100

    add board display + coordinates logic

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
-func printBoard(arr [3][3]string) {
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			print(arr[i][j])
+import "fmt"
+
+func putSymbol(arr [3][3]string, symbol string, x int, y int) [3][3]string {
+	arr[x][y] = symbol
+	return arr
+}
+
+func askCoordinates(x *int, y *int) {
+	var a, b int
+	for true {
+		println("Enter the number of the row for your next move (0-2)")
+		fmt.Scanln(&a)
+		if a < 0 || a > 2 {
+			break
 		}
-		println()
+		println("Enter the number of the column for your next move (0-2)")
+		fmt.Scanln(&b)
+		if b < 0 || b > 2 {
+			break
+		}
 	}
+
+	*x = a
+	*y = b
 }
 
 func checkDiagonals(arr [3][3]string) bool {
@@ -45,21 +62,19 @@ func checkVerticals(arr [3][3]string) bool {
 	return false
 }
 
-func displayBoard() {
-
+func displayBoard(arr [3][3]string) {
+	println(arr[0][0], "|", arr[0][1], "|", arr[0][2])
+	println(arr[1][0], "|", arr[1][1], "|", arr[1][2])
+	println(arr[2][0], "|", arr[2][1], "|", arr[2][2])
 }
-
-// 0 | 0 | 0 |
-// 0 | 0 | 0 |
-// 0 | 0 | 0 |
 
 func main() {
 	println("Welcome to TicTacToe Game")
 
-	//board := [3][3]string{{"", "", ""}, {"", "", ""}, {"", "", ""}}
-	boardTest := [3][3]string{{"X", "X", "O"}, {"", "", ""}, {"", "", ""}}
+	board := [3][3]string{{"_", "_", "_"}, {"_", "_", "_"}, {"_", "_", "_"}}
+	//boardTest := [3][3]string{{"X", "X", "O"}, {"", "", ""}, {"", "", ""}}
 
-	res := checkHorizontals(boardTest)
-	println(res)
+	//res := checkHorizontals(boardTest)
+	displayBoard(board)
 
 }
